41f8004f302e827043b7675cee550d7f8fa6bc82
사공지은 <gonii0821@naver.com>
2023-03-05 22:36:42 +0900

    todo 수정 및 삭제 구현

--- react-ts-vite-practice/src/components/TodoListItem.tsx
+++ react-ts-vite-practice/src/components/TodoListItem.tsx
@@ -1,8 +1,9 @@
-import * as React from "react";
+import { useState } from "react";
 import { css } from "@emotion/react";
 import modifyButton from "./../assets/images/modifyButton.png";
 import deleteButton from "./../assets/images/deleteButton.png";
-import { ITodoTypes } from "../recoil/atom";
+import { ITodoTypes, todoState } from "../recoil/atom";
+import { useRecoilState } from "recoil";
 
 export interface ITodoListItemProps {
   key: number;
@@ -10,15 +11,55 @@ export interface ITodoListItemProps {
 }
 
 export default function TodoListItem(props: ITodoListItemProps) {
+  const [todo, setTodo] = useRecoilState<ITodoTypes[]>(todoState);
+  const [modifyMode, setModifyMode] = useState<boolean>(false);
+  const [newContent, setNewContent] = useState<string>(props.todo.contents);
+  const index = todo.findIndex((listItem) => listItem === props.todo);
+
+  const onClickModifyTodo = (): void => {
+    setModifyMode(!modifyMode);
+  };
+
+  const onClickDeleteTodo = (): void => {
+    const newList = [...todo.slice(0, index), ...todo.slice(index + 1)];
+    setTodo(newList);
+  };
+
+  const onChangeTodo = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setNewContent(e.target.value);
+  };
+
+  const onKeyDownEnter = (e: React.KeyboardEvent<HTMLInputElement>, id: number): void => {
+    if (e.key === "Enter") {
+      const modifiedTodo = {
+        ...props.todo,
+        contents: newContent,
+      };
+
+      const newList = [...todo.slice(0, index), modifiedTodo, ...todo.slice(index + 1)];
+      setTodo(newList);
+
+      setModifyMode(!modifyMode);
+    }
+  };
+
   return (
     <div css={listItemWrapper}>
       <div css={leftSection}>
         <input type="checkbox" />
-        <span>{props.todo.contents}</span>
+        {modifyMode ? (
+          <input
+            placeholder={props.todo.contents}
+            onChange={(e) => onChangeTodo(e)}
+            onKeyDown={(e) => onKeyDownEnter(e, props.todo.id)}
+          />
+        ) : (
+          <span>{props.todo.contents}</span>
+        )}
       </div>
       <div css={rightSection}>
-        <img src={modifyButton} />
-        <img src={deleteButton} />
+        <img src={modifyButton} onClick={onClickModifyTodo} />
+        <img src={deleteButton} onClick={onClickDeleteTodo} />
       </div>
     </div>
   );
@@ -36,7 +77,7 @@ const leftSection = css`
   display: flex;
   align-items: center;
 
-  > input {
+  > input:nth-of-type(1) {
     margin-right: 15px;
     width: 35px;
     height: 35px;
@@ -44,6 +85,16 @@ const leftSection = css`
     border: 2px solid #66dd9c;
     border-radius: 5px;
     cursor: pointer;
+  }
+
+  > input:nth-of-type(2) {
+    font-size: 18px;
+    color: #000000;
+    border: none;
+    border-bottom: 1px solid #989595;
+    outline: none;
+    width: 550px;
+    padding: 6px 0;
   }
 
   > span {
@@ -55,10 +106,6 @@ const leftSection = css`
 const rightSection = css`
   display: flex;
   align-items: center;
-
-  /* > img:nth-of-type(1) {
-    margin-right: 15px;
-  } */
 
   > img {
     margin-right: 15px;
